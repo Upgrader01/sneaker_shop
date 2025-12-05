bb74de115183228227a8799f0f6345aba066cd39
Upgrader01 <ad5055874@gmail.com>
2025-12-05 12:35:41 +0200

    Create Product model

---
+++ lib/models/product.dart
@@ -0,0 +1,25 @@
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String image;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.price,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      image: json['image'],
+      price: (json['price'] as num).toDouble(),
+    );
+  }
+}
